3584efb6900f2eb70f1058bdb1196d3f236f652a
Doe Agudey Daniel <agudeydaniel8@gmail.com>
2024-12-29 05:26:42 +0000

    committing after initial got corrupted

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const { PORT } = require("./config/env.config");
+const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+
+// Routes
+app.use(authRoutes);
+app.use(productRoutes);
+app.use(orderRoutes);
+
+// Default error handler
+app.use((err, req, res, next) => {
+  console.error("Unhandled error:", err);
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
+
+// Start server and export app
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`API Gateway running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
